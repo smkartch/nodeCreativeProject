f8a3254d0c9af1cefda3215c01a29100d7395558
EC2 Default User <ec2-user@ip-172-31-16-226.us-east-2.compute.internal>
2019-11-07 23:19:34 +0000

    Finished

---
+++ public/javascripts/script.js
@@ -0,0 +1,19 @@
+/*global fetch
+global Vue*/
+    
+document.getElementById("myButton").addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    const url = "https://api.thecatapi.com/v1/images/search";
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            console.log(json);
+            let results = "";
+            results += "<img src=\"" + json[0].url + "\" id=\"catImage\"></img>";
+            console.log(json[0].url);
+            document.getElementById("catFact").innerHTML = results;
+        });
+});
+
